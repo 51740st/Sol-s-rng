6a561e6c932bfb9111361a88c468e7d93c7ce6d9
plis <144231877+51740st@users.noreply.github.com>
2024-03-15 09:54:06 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 15532962292 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/51740st/Sol-s-rng/main/test.lua"))()
+else
+print("This game is not supported")
+end
